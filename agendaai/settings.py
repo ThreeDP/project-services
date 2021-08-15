d2fa8633334fea31e30542d88bc9ee9c8b950307
Threedp <davypaulinocbjr@outlook.com>
2021-08-15 20:48:24 -0300

    Novas atualizações para o heroku

--- agendaai/settings.py
+++ agendaai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = open('key.txt')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
